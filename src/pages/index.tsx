febf2c747a263297fc1a03196b40b33a922b6da5
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-03-01 13:17:57 +0700

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const HomePage = () => (
+  <div style={{ maxWidth: 640, margin: "3rem auto" }}>
+    <h1>Phat Tran's CV</h1>
+    <p>Phat yeu Hang</p>
+  </div>
+)
+
+export default HomePage
